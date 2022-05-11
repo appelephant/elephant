ff158a4b8b1c2c405c2ed7fd921f992127b44836
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-05-11 02:48:19 -0300

    chore: formatter :pencil2:

--- lib/elephant/release.ex
+++ lib/elephant/release.ex
@@ -14,14 +14,17 @@ defmodule Elephant.Release do
     ensure_started()
 
     for repo <- repos() do
-      {:ok, _fun_return, _apps} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _fun_return, _apps} =
+        Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   @spec rollback(atom(), any()) :: {:ok, any, any}
   def rollback(repo, version) do
     load_app()
-    {:ok, _fun_return, _apps} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+
+    {:ok, _fun_return, _apps} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   @spec seed :: [{:ok, any, any}]
